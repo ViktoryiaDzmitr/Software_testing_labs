21975f1fc0b3e80ae1effc79bfea85b8504331c7
Viktroyia Dzmitrachonak <viktoryia.dzmitrachonak@leverx.com>
2018-10-28 23:05:04 +0300

    Added PageFactory Object

---
+++ LabPageFactory/LabPageFactory/ClassPageFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace LabPageFactory
+{
+    class ClassPageFactory
+    {
+
+        public ClassPageFactory()
+        {
+            PageFactory.InitElements(Program.browser, this);
+
+        }
+
+        [FindsBy(How = How.Id, Using = "from_name")]
+        public IWebElement Place_from;
+
+
+        [FindsBy(How = How.Id, Using = "to_name")]
+        public IWebElement Place_to;
+
+        [FindsBy(How = How.Id, Using = "departure_date")]
+        public IWebElement DepartureDate_first;
+
+        [FindsBy(How = How.ClassName, Using = "date_1541970000000")]
+        public IWebElement Calendar_datefirst;
+
+        [FindsBy(How = How.Id, Using = "departure_date_")]
+        public IWebElement DepartureDate_second;
+
+        [FindsBy(How = How.ClassName, Using = "date_1541710800000")]
+        public IWebElement Calendar_datesecond;
+
+        [FindsBy(How = How.ClassName, Using = "search_button")]
+        public IWebElement SearchButton;
+
+    }
+}
---
+++ LabPageFactory/LabPageFactory/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace LabPageFactory
+{
+    class Program
+    {
+        public static IWebDriver browser;
+
+
+        static void Main(string[] args)
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl("http://avia.321.by/");
+            ClassPageFactory pageObject = new ClassPageFactory();
+
+            pageObject.Place_from.Clear();
+            pageObject.Place_from.SendKeys("Минск");
+            System.Threading.Thread.Sleep(1000);
+            pageObject.Place_from.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+
+            pageObject.DepartureDate_first.Click();
+            System.Threading.Thread.Sleep(1000);
+            pageObject.Calendar_datefirst.Click();
+
+            try
+            {
+                pageObject.Place_to.Clear();
+                pageObject.Place_to.SendKeys("Минск");
+                System.Threading.Thread.Sleep(1000);
+                pageObject.Place_to.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+
+                pageObject.DepartureDate_second.Click();
+                System.Threading.Thread.Sleep(1000);
+                pageObject.Calendar_datesecond.Click();
+            }
+            catch {
+                Console.WriteLine("Input Error.");
+            }
+
+            pageObject.SearchButton.Click();
+        }
+    }
+}
